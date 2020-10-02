83faa4d7c95032e995d579d0cbbf12917fd66f97
JP_Billy <jannykarper@gmail.com>
2020-10-02 10:06:22 +0300

    02_10_20

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Profile = (props) => {
-  let user = props.function();
+  let userId = window.location.pathname.split("/")[2];
+  let user = props.function(userId);
   return (<div className="row">
     <div className="col-sm-3">
       <img src={user.avatar} width="90%" alt=""/>
     </div>
     <div className="col-sm-9">
-      <h1>{user.name}
-        {user.lastname}</h1>
+      <h1>{user.name} {user.lastname}</h1>
       <h4>Обо мне</h4>
       <p>{user.about}</p>
     </div>
--- src/state.jsx
+++ src/state.jsx
@@ -1,30 +1,78 @@
 const user = {
-    name: "Name",
-    lastname : "Lastname",
-    email: "name@lst.na",
-    id: "1",
-    about: "Somthing about",
-    avatar: "https://s1.iconbird.com/ico/2013/12/505/w450h4001385925286User.png"
+  name: "Name",
+  lastname: "Lastname",
+  email: "name@lst.na",
+  id: "1",
+  about: "Somthing about",
+  avatar: "https://s1.iconbird.com/ico/2013/12/505/w450h4001385925286User.png"
 }
 
 const users = {
-    0: {name: "Марина", lastname: "Крутелева"},
-    1: {name: "Марина", lastname: "Солдатова"},
-    2: {name: "Ева", lastname: "Жолдина"},
-    3: {name: "Нона", lastname: "Мандрыкина"},
-    4: {name: "Ева", lastname: "Мандрыкина"},
-    5: {name: "Венедикт", lastname: "Солдатов"},
-    6: {name: "Нона", lastname: "Язина"},
-    7: {name: "Сигизмунд", lastname: "Моряев"},
-    8: {name: "Ульяна", lastname: "Иньшова"},
-    9: {name: "Юнона", lastname: "Крутелева"},
-    10:{name: "Самсон", lastname: "Мандрыкин"}
+  0: {
+    name: "Марина",
+    lastname: "Крутелева",
+    id: 10
+  },
+  1: {
+    name: "Марина",
+    lastname: "Солдатова",
+    id: 11
+  },
+  2: {
+    name: "Ева",
+    lastname: "Жолдина",
+    id: 12
+  },
+  3: {
+    name: "Нона",
+    lastname: "Мандрыкина",
+    id: 13
+  },
+  4: {
+    name: "Ева",
+    lastname: "Мандрыкина",
+    id: 14
+  },
+  5: {
+    name: "Венедикт",
+    lastname: "Солдатов",
+    id: 15
+  },
+  6: {
+    name: "Нона",
+    lastname: "Язина",
+    id: 16
+  },
+  7: {
+    name: "Сигизмунд",
+    lastname: "Моряев",
+    id: 17
+  },
+  8: {
+    name: "Ульяна",
+    lastname: "Иньшова",
+    id: 18
+  },
+  9: {
+    name: "Юнона",
+    lastname: "Крутелева",
+    id: 19
+  },
+  10: {
+    name: "Самсон",
+    lastname: "Мандрыкин",
+    id: 20
+  }
 }
 
-export function getUser(){
-    return user;
+export function getUser(userId) {
+  for (let i = 0; i < Object.keys(user).length; i++) {
+    if (users[i].id == userId)
+      return users[i];
+    }
+  return user;
 }
 
-export function getUsers(){
-    return users;
+export function getUsers() {
+  return users;
 }
